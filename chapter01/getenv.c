da55b341b14c36ce191ea94899e277f9b40335d1
Zivana Syahputri <zivanacs18@gmail.com>
2024-02-26 20:27:02 -0500

    Revert "Adding chapter 4 notes.txt"
    This reverts commit 6e7688b6a077f954aae712ce2ada91cca50a2213.

--- chapter01/getenv.c
+++
@@ -1,7 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-
-void main() {
-    char* num_columns = getenv("COLUMNS");
-    printf("This window has %s columns.\n", num_columns);
-}
--- chapter01/gettid_demo.c
+++
@@ -1,9 +0,0 @@
-// obtaining thread id
-#define _GNU_SOURCE
-#include <stdio.h>
-#include <sys/types.h>
-#include <unistd.h>
-
-int main() {
-    printf("The thread id number is %d.\n", gettid());
-}
