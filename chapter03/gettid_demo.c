e6d6a34d8ca86e892612295d8f31041327f36008
Zivana Syahputri <zivanacs18@gmail.com>
2024-05-03 13:39:24 -0400

    Merged to demos

--- chapter03/gettid_demo.c
+++
@@ -1,9 +0,0 @@
-#include <unistd.h>
-#include <sys/syscall.h>
-#include <sys/types.h>
-#include <stdio.h>
-
-int main(int argc, char *argv[]) {
-    printf("Thread id %ld\n", syscall(SYS_gettid));
-    return 0;
-}
