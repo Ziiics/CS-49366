117353605b3aef71a22d15cd6578442f38f4fe35
NightFury <121653635+Ziiics@users.noreply.github.com>
2024-05-08 19:22:52 -0400

    Update winsize.c

--- demos/chapter10/winsize.c
+++ demos/chapter10/winsize.c
@@ -1,22 +1,32 @@
 /*****************************************************************************
-  Title          : winsize.c
-  Author         : Stewart Weiss
-  Created on     : March 10, 2008
   Description    : Displays window size in lines and rows (pixels usually fails)
   Purpose        : To demonstrate the ioctl() call
   Usage          : winsize
   Build with     : gcc -Wall -g -o winsize winsize.c
-
-******************************************************************************
-* Copyright (C) 2024 - Stewart Weiss                                         *
-*                                                                            *
-* This code is free software; you can use, modify, and redistribute it       *
-* under the terms of the GNU General Public License as published by the      *
-* Free Software Foundation; either version 3 of the License, or (at your     *
-* option) any later version. This code is distributed WITHOUT ANY WARRANTY;  *
-* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A      *
-* PARTICULAR PURPOSE. See the file COPYING.gplv3 for details.                *
 *****************************************************************************/
+
+/*
+
+TIOSCGWINSZ - a macro used with 'ioctl' to get te window size of the terminal
+
+struct winsize {
+  unsigned short ws_row;	/* rows, in characters */
+  unsigned short ws_col;	/* columns, in characters */
+  unsigned short ws_xpixel;	/* horizontal size, pixels */
+  unsigned short ws_ypixel;	/* vertical size, pixels */
+};
+  #include <sys/ioctl.h>
+  // int ioctl(int fd, int cmd, ... );
+
+int ioctl(int fd, unsigned long request, ...);
+  // manipulates the underlying device parameters of special files
+  int fd - file descriptor that represents an open file, socket, or other devices that supports device-specific I/O operations
+  unsigned long request - device-dependent request code that determine the behavior of 'ioctl()'
+  ... (varags) - optional arguments whose type and number depend on the 'request' code
+  
+*/
+
+
 #include "common_hdrs.h"
 #include <termios.h>
 
@@ -25,7 +35,7 @@
 #include <sys/ioctl.h>
 #endif
 
-void get_winsize(int fd, unsigned short *rows, unsigned short *cols )
+void get_winsize(int fd, unsigned short *rows, unsigned short *cols)
 {
     struct winsize size;
 
@@ -36,7 +46,6 @@ void get_winsize(int fd, unsigned short *rows, unsigned short *cols )
     *rows = size.ws_row;
     *cols = size.ws_col;
 }
-
 
 /*****************************************************************************
                                Main Program
