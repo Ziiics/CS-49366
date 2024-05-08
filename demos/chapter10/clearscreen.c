61c868e4583b072477d7927a94d91d8aa77770a7
NightFury <121653635+Ziiics@users.noreply.github.com>
2024-05-08 10:53:59 -0400

    Update clearscreen.c

--- demos/chapter10/clearscreen.c
+++ demos/chapter10/clearscreen.c
@@ -1,28 +1,25 @@
 /*****************************************************************************
-  Title          : clearscreen.c
-  Author         : Stewart Weiss
-  Created on     : January  2, 2011
   Description    : Clears the terminal window using ANSI escape
   Purpose        : To show how to use an ANSI escape sequence
   Usage          : clearscreen
   Build with     : gcc -o clearscreen -I../include clearscreen.c
-
-******************************************************************************
-* Copyright (C) 2023 - Stewart Weiss                                         *
-*                                                                            *
-* This code is free software; you can use, modify, and redistribute it       *
-* under the terms of the GNU General Public License as published by the      *
-* Free Software Foundation; either version 3 of the License, or (at your     *
-* option) any later version. This code is distributed WITHOUT ANY WARRANTY;  *
-* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A      *
-* PARTICULAR PURPOSE. See the file COPYING.gplv3 for details.                *
 *****************************************************************************/
+
+/*
+
+#define CLEAR_SCREEN "\033[2J" 
+  // define macro named 'CLEAR_SCREEN', using the sequence '\033[2J' which is recognnized by terminal emulators
+  // \033: ASCII escape character, represents the beginning of an escape sequence
+     [: opening bracket, marking the start of a control sequence.
+     2J: control sequence parameter that tells the terminal to clear the screen.
+*/
+
 
 #define _GNU_SOURCE
 #include "common_hdrs.h"
-#include <termios.h>
+#include <termios.h>  // provide temrinal control fucntions and data structures
 
-#define CLEAR_SCREEN    "\033[2J"
+#define CLEAR_SCREEN    "\033[2J" 
 
 int main (int argc, char *argv[])
 {
