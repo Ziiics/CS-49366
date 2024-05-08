0c088cfd7f80b4b90de8e9776a720b82a7fd61fa
NightFury <121653635+Ziiics@users.noreply.github.com>
2024-05-08 10:29:38 -0400

    Update alarm_demo1.c

--- demos/chapter10/alarm_demo1.c
+++ demos/chapter10/alarm_demo1.c
@@ -1,7 +1,4 @@
 /*****************************************************************************
-  Title       : alarm_demo1.c
-  Author      : Stewart Weiss
-  Created on  : December 19, 2023
   Description : Calls alarm, pause using the sigaction() to install handler
   Purpose     :
   Usage       : alarmdemo2  <numseconds>
@@ -9,17 +6,8 @@
                        set the alarm
   Build with  : gcc -Wall -g -I../include -L../lib -o alarm_demo1  \
                   alarm_demo1.c -lspl
-
-******************************************************************************
-* Copyright (C) 2023 - Stewart Weiss                                         *
-*                                                                            *
-* This code is free software; you can use, modify, and redistribute it       *
-* under the terms of the GNU General Public License as published by the      *
-* Free Software Foundation; either version 3 of the License, or (at your     *
-* option) any later version. This code is distributed WITHOUT ANY WARRANTY;  *
-* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A      *
-* PARTICULAR PURPOSE. See the file COPYING.gplv3 for details.                *
 *****************************************************************************/
+
 #include "common_hdrs.h"
 #include <signal.h>
 
@@ -32,7 +20,7 @@ int main(int argc, char * argv[])
         usage_error("alarm_demo1 <alarm-interval>");
     }
 
-    resultcode = get_int(argv[1], NON_NEG_ONLY|PURE, &k, NULL );
+    resultcode = get_int(argv[1], NON_NEG_ONLY|PURE, &k, NULL ); // convert user's arg into int
     if ( resultcode < 0 || k < 1 )
         fatal_error(resultcode, "get_int expects a positive integer");
 
