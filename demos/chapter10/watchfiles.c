74f0b4b840bb31ebb9ed4e137b1ee1233ca1a78c
NightFury <121653635+Ziiics@users.noreply.github.com>
2024-05-09 14:02:00 -0400

    Update watchfiles.c

--- demos/chapter10/watchfiles.c
+++ demos/chapter10/watchfiles.c
@@ -1,22 +1,9 @@
 /*****************************************************************************
-  Title          : watchfiles.c
-  Author         : Stewart Weiss
-  Created on     : February 16, 2024
   Description    : Displays files and their sizes, updating dynamically
   Purpose        : To show how to work with timers
   Usage          : watchfiles  file file ...
   Build with     : gcc -Wall -g -I ../include watchfiles.c -o watchfiles \
                    -L../lib -lspl -lrt
-
-******************************************************************************
-* Copyright (C) 2023 - Stewart Weiss                                         *
-*                                                                            *
-* This code is free software; you can use, modify, and redistribute it       *
-* under the terms of the GNU General Public License as published by the      *
-* Free Software Foundation; either version 3 of the License, or (at your     *
-* option) any later version. This code is distributed WITHOUT ANY WARRANTY;  *
-* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A      *
-* PARTICULAR PURPOSE. See the file COPYING.gplv3 for details.                *
 *****************************************************************************/
 
 
